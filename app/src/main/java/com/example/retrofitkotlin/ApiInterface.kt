91f73e91481031eba73df3fc395db87fa831e54f
Enes Mahmut ÇAKMAK <enescakmak44@gmail.com>
2020-05-18 00:25:52 +0300

    Update ApiInterface.kt

--- app/src/main/java/com/example/retrofitkotlin/ApiInterface.kt
+++ app/src/main/java/com/example/retrofitkotlin/ApiInterface.kt
@@ -6,9 +6,6 @@ import retrofit2.http.Query
 
 interface ApiInterface {
 
-//    @GET("movie/{id}")
-//    fun getAllPlaylist(@Path("id") id:Int, @Query("api_key") apiKey:String):Call<Data>
-
     @GET("movie/popular")
     fun getAllPlaylist(@Query("api_key") apiKey:String):Call<Data>
 }
